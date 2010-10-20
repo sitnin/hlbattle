c08f5f37dfe8b675eedb97566a16ca6af2e41b12
Gregory Sitnin <gregor@gregor.ru>
2010-10-20 18:35:20 +0400

    Unicode bugfix

--- webapp.py
+++ webapp.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import locale, sys
+locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 import os
 import config
 import tornado.httpserver
