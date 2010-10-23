16eb3cae471f1e1089a1dd0f96ab0acad2372ece
Gregory Sitnin <gregor@gregor.ru>
2010-10-24 03:51:52 +0400

    Incomplete data loader

--- datagen/datagen.py
+++ datagen/datagen.py
@@ -7,19 +7,36 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 import os
+import fnmatch
+import random
 
 
-def load_data():
-    pass
+def load_data(path):
+    data = list()
+    for filename in fnmatch.filter(os.listdir(path), "*.txt"):
+        with open(path+"/"+filename, "r") as f:
+            lines = f.read().splitlines()
+            f.close()
+        data.append((lines[0], lines[1:]))
+    return data
 
-def run():
-    pass
+def run(count, data):
+    def get_content(item):
+        text = ""
+        title = item[0]
+        for s in item[1]:
+            text += s+"\n"
+        return (title, text)
+
+    for x in xrange(count):
+        
     
-def print_result():
+    for item in data:
+        print(get_content(item))
+    
+def print_result(res):
     pass
 
 
 if __name__ == "__main__":
-    load_data()
-    run()
-    print_result()
+    print_result(run(100, load_data("data/")))
